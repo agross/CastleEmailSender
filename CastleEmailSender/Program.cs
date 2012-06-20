79de618fdbbd061d9ea570bed48e590b3e81119e
Alexander Groß <agross@therightstuff.de>
2012-06-21 01:04:54 +0200

    Inital commit

---
+++ CastleEmailSender/Program.cs
@@ -0,0 +1,17 @@
+﻿using Castle.Core.Smtp;
+
+namespace CastleEmailSender
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var sender = new DefaultSmtpSender();
+
+      // Things that get reset from the auto-configuring SmtpClient:
+      // - Port (587 in config, 25 wehn debugging)
+      // - Credentials (user/pass in config, default creds when debugging)
+      sender.Send("you@googlemail.com", "example@example.com", "Subject", "Text");
+    }
+  }
+}
